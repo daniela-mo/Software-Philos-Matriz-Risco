b44892d6475d85b6df1eb62446e60deac914adaa
philos-tec <113989915+philos-tec@users.noreply.github.com>
2023-01-20 14:15:02 -0300

    Delete v-11.0 directory

--- v-11.0/teste2.com.br/conexao.php
+++
@@ -1,1 +0,0 @@
- <?php setlocale(LC_TIME, 'pt_BR', 'pt_BR.utf-8', 'pt_BR.utf-8', 'portuguese'); ?> <?php date_default_timezone_set('America/Sao_Paulo'); ?><?php $conexao= mysqli_connect('50.19.17.159', 'root', 'philos.tec','sistemam2vconsul_teste2'); ?>
--- v-11.0/upload-arquivos-rnc.php
+++
@@ -1,3 +0,0 @@
-<span id="msg" style="color:red"></span><br />
-
-<input type="file" id="photo" name="photo" class="mb-3" multiple><br />
